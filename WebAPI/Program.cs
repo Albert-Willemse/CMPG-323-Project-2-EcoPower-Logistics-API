269ba51855eac469d078974ce2afa2e4255e5bca
ALBERT WILLEMSE <38205742@student365.msfed.nwu.ac.za>
2023-08-19 17:18:05 +0200

    Apply dependency injection

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,8 +1,16 @@
+using WebAPI.Models; // Using the models
+using Microsoft.EntityFrameworkCore; // Reference the models that i am using
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Dependency injection, therefore we need to reference the connection string with the dbcontext
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");  
+builder.Services.AddDbContext<EcoPowerSolutionsDBContext>(options => options.UseSqlServer(connectionString));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
